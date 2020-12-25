df5e8de02368f574fb85036c52dff77d6ab49370
root <root@ip-172-31-13-130.ap-south-1.compute.internal>
2020-12-25 11:40:28 +0000

    fixing shell script issue

--- addinguser.sh
+++ addinguser.sh
@@ -3,10 +3,10 @@
 
 echo "---STRING parameter-----"
 
-sudo useradd -d /home/$username -m $username
+sudo useradd kamal2
 
 echo "----Choice Parameter----"
 
-sudo useradd $username3 -d /home/$username3 -m $username3
-
-id $username3
+sudo useradd ajay2
+id ajay2
+id kamal2
